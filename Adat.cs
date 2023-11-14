74b82710a4dd821f9b245e3ead59c0ee2336b919
Szima Ákos <70635906+k3tchupp@users.noreply.github.com>
2023-11-14 12:17:12 +0100

    Add files via upload

---
+++ Adat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotellift
+{
+    internal class Adat
+    {
+        public int Ev { get; set; }
+        public int Honap { get; set; }
+        public int Nap { get; set; }
+        public int Sorszam { get; set; }
+        public int InduloSzint { get; set; }
+        public int CelSzint { get; set; }
+
+        public Adat(int Ev, int Honap, int Nap, int Sorszam, int InduloSzint, int CelSzint)
+        {
+            this.Ev = Ev;
+            this.Honap = Honap;
+            this.Nap = Nap;
+            this.Sorszam = Sorszam;
+            this.InduloSzint = InduloSzint;
+            this.CelSzint = CelSzint;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿public List<Adat> lista = new List<Adat>();
+
+using(StreamReader sr = new StreamReader("lift.exe"))
+{
+    while (!sr.EndOfStream)
+    {
+        sr.ReadLine();
+        // eddig jutottam
+    }
+}
